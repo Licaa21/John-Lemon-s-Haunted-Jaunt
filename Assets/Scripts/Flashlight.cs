c2a5cd27999c91d610b55bfc88bf3697f15c8442
Licaa21 <lica.albert.u9a@student.ucv.ro>
2025-05-09 09:00:09 +0300

    Initial commit

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    public Light flashlightLight;   // Referință la obiectul Light (lanterna)
+    public KeyCode toggleKey = KeyCode.F;  // Tasta care va activa/dezactiva lanterna
+
+    void Start()
+    {
+        // Asigură-te că lanterna este oprită la început
+        flashlightLight.enabled = false;
+    }
+
+    void Update()
+    {
+        // Când se apasă tasta F, schimbăm starea lanternei
+        if (Input.GetKeyDown(toggleKey))
+        {
+            flashlightLight.enabled = !flashlightLight.enabled;  // Toggle (pornit/oprit) lanterna
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float turnSpeed = 20f;
+    public float walkSpeedMultiplier = 1f;
+    public float sprintSpeedMultiplier = 2f;
+    public float walkAnimSpeed = 1f;
+    public float sprintAnimSpeed = 1.75f;
+    public float walkAudioPitch = 1f;
+    public float sprintAudioPitch = 1.4f;
+
+    Animator m_Animator;
+    Rigidbody m_Rigidbody;
+    AudioSource m_AudioSource;
+    Vector3 m_Movement;
+    Quaternion m_Rotation = Quaternion.identity;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+        m_AudioSource = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        bool isSprinting = Input.GetKey(KeyCode.LeftShift);
+
+        float speedMultiplier = isSprinting ? sprintSpeedMultiplier : walkSpeedMultiplier;
+        float animSpeed = isSprinting ? sprintAnimSpeed : walkAnimSpeed;
+        float audioPitch = isSprinting ? sprintAudioPitch : walkAudioPitch;
+
+        m_Movement.Set(horizontal, 0f, vertical);
+        m_Movement = m_Movement.normalized * speedMultiplier;
+
+        bool hasHorizontalInput = !Mathf.Approximately(horizontal, 0f);
+        bool hasVerticalInput = !Mathf.Approximately(vertical, 0f);
+        bool isWalking = hasHorizontalInput || hasVerticalInput;
+        m_Animator.SetBool("IsWalking", isWalking);
+        m_Animator.speed = animSpeed;
+
+        if (isWalking)
+        {
+            if (!m_AudioSource.isPlaying)
+            {
+                m_AudioSource.Play();
+            }
+            m_AudioSource.pitch = audioPitch;
+        }
+        else
+        {
+            m_AudioSource.Stop();
+        }
+
+        Vector3 desiredForward = Vector3.RotateTowards(transform.forward, m_Movement, turnSpeed * Time.deltaTime, 0f);
+        m_Rotation = Quaternion.LookRotation(desiredForward);
+    }
+
+    void OnAnimatorMove()
+    {
+        m_Rigidbody.MovePosition(m_Rigidbody.position + m_Movement * m_Animator.deltaPosition.magnitude);
+        m_Rigidbody.MoveRotation(m_Rotation);
+    }
+}
